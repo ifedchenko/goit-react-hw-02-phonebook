e35528736a4737d2a685501d7986762014c1f55c
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-07-04 16:37:01 +0300

    Finish task Phonebook

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { ContainerBlock } from './Container.styled';
+
+export const Container = ({ children }) => {
+  return <ContainerBlock>{children}</ContainerBlock>;
+};
